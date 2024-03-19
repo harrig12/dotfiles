5d0495c8dd33ddb87d7298443d9b8cd113a93709
Caitlin Harrigan <harrigc@ca124.camp.thecrick.org>
2024-03-19 15:55:52 +0000

    rm lim

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,17 +5,3 @@ alias ll='ls -l --color=auto'
 alias la='ls -a --color=auto'
 alias lla='ls -la --color=auto'
 
-# type ssh pw
-alias ssh-verif='eval `ssh-agent`; ssh-add'
-
-# rsub
-alias rsub='~/rsub'
-
-<<<<<<< HEAD
-# windows
-export lime='/mnt/c/Users/caith/'
-
-# R
-alias R='cd $lime; R.exe --no-save'
-alias Rscript='cd $lime; Rscript.exe'
-
