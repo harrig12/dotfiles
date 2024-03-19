d5424d694e15dbc8e35dac135e268218473db6d5
Caitlin Harrigan <harrigc@ca124.camp.thecrick.org>
2024-03-19 15:59:57 +0000

    update gac

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,6 +9,6 @@ alias gd='git diff --color'
 alias gr='git rm -r'
 alias ga='git add'
 alias gc='git commit -m'
-alias gac='git commit -am'
+alias gac='git add -A && git commit -am'
 alias gcb='git checkout'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
